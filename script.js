b0d7bd69d0c9ca5d37036523ab427f71b7e7f697
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2021-04-09 12:46:51 +0600

    final commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// Selection 
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+
+// Initial Date
+const newYears = '1 May 2021';
+
+// Function for Countdown
+function countDown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    // Calculation for Days, Hours, Minute, Seconds, 
+    const days = Math.floor(totalSeconds / 3600 / 24)
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    // Display Days, Hours, Minutes, Seconds
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+}
+countDown();
+
+// time interval
+
+setInterval(countDown, 1000)
